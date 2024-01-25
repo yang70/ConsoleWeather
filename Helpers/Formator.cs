d47e33ceaf85bee740da3f946fcad13d40e0c31e
Matthew Yang <myang@costar.com>
2024-01-25 11:41:23 -0800

    Add weather images

--- Helpers/Formator.cs
+++ Helpers/Formator.cs
@@ -9,6 +9,7 @@ namespace Helpers
 			string location = weatherData.RootElement.GetProperty("name").GetString() ?? "Unknown location";
 			string weatherShort = weatherData.RootElement.GetProperty("weather")[0].GetProperty("main").GetString() ?? "Unknown weather";
 			string weatherDescription = weatherData.RootElement.GetProperty("weather")[0].GetProperty("description").GetString() ?? "Unknown description";
+			string iconId = weatherData.RootElement.GetProperty("weather")[0].GetProperty("icon").GetString() ?? "none";
 			float temp = weatherData.RootElement.GetProperty("main").GetProperty("temp").GetSingle();
 			float feelsLike = weatherData.RootElement.GetProperty("main").GetProperty("feels_like").GetSingle();
 			float windSpeed = weatherData.RootElement.GetProperty("wind").GetProperty("speed").GetSingle();
@@ -19,6 +20,9 @@ namespace Helpers
 			formattedString += $"{weatherShort} - {weatherDescription}\n";
 			formattedString += $"{temp}°F (feels like {feelsLike}°F)\n";
 			formattedString += $"Wind: {windSpeed} mph\n";
+
+			var picture = Path.Combine(Directory.GetCurrentDirectory(), $"Pics/{iconId}.txt");
+			formattedString += File.ReadAllText(picture);
 
 			return formattedString;
 		}
